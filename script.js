1df7aa89ae9ed51499cebf06f57ddcccf11b7a5a
Matheus <matheusdugoncalves@gmail.com>
2024-03-17 21:17:17 -0300

    git commit -a

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ document.addEventListener("DOMContentLoaded", function () {
   // Light mode from 5 AM to 6 PM
   if (hour >= 5 && hour < 18) {
     html.classList.add("light")
+    applyModeStyles("light")
   } else {
     html.classList.remove("light")
+    applyModeStyles("dark")
   }
 
   // Função para aplicar estilos do modo claro ou escuro
@@ -16,16 +18,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const img = document.querySelector("#footer img")
     // Determina o caminho do arquivo SVG com base no modo
     const svgPath =
-      mode === "light" ? "./assets/Logo-dark.svg" : "./assets/Logo-light.svg"
+      mode === "light" ? "/assets/Logo-light.svg" : "/assets/Logo-dark.svg"
 
-    // Verifica se o navegador é o Safari
-    const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
-    // Se for o Safari, define o atributo src da imagem para o modo correto
-    if (isSafari) {
-      img.setAttribute("src", svgPath)
-    }
+    // Define o atributo src da imagem para o modo correto
+    img.setAttribute("src", window.location.origin + svgPath)
   }
-
-  // Aplica estilos com base no modo atual
-  applyModeStyles(html.classList.contains("light") ? "light" : "dark")
 })
